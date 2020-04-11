087d7882b20a9a5393839a4577ed893f344ef4ba
pinmode-by <54795176+pinmode-by@users.noreply.github.com>
2020-04-11 20:09:29 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sensors for uBlitz 2 (Line following robot)
+
+## License
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
---
+++ main.ts
@@ -0,0 +1,126 @@
+
+//% color=#FF0000 weight=8 icon="\uf0fb"
+namespace microBlitz{
+
+let SENS_NUM:number=8;
+let sensors: Array<number>;
+sensors = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; 
+  
+   //% block="sensors Init N %snum"
+   //% block.loc.ru="Инициализация датчиков N %snum"
+   //% snum.min=1 snum.max=16
+   //% snum.defl=8
+   //% weight=90
+  export function sensorsInit( snum: number) { 
+      SENS_NUM=snum
+  }
+    
+
+   //% block="readSensors"
+   //% block.loc.ru="Чтение всех датчиков"
+   //% weight=90
+  export function readSensors() { 
+         
+      for (let i = 0; i < SENS_NUM; i++) {
+         let N:number = i
+         if (N >= 8) {
+            N += -8
+            a3 = 1
+        } else {
+            a3 = 0
+        }
+          
+        if (N >= 4) {
+            N += -4
+            a2 = 1
+        } else {
+            a2 = 0
+        }
+          
+        if (N >= 2) {
+            N += -2
+            a1 = 1
+        } else {
+            a1 = 0
+        }
+          
+        if (N >= 1) {
+            a0 = 1
+        } else {
+            a0 = 0
+        }
+        pins.digitalWritePin(DigitalPin.P0, a0)
+        pins.digitalWritePin(DigitalPin.P1, a1)
+        pins.digitalWritePin(DigitalPin.P2, a2)
+        pins.digitalWritePin(DigitalPin.P3, a3)  
+        sensors[i]=pins.analogReadPin(AnalogPin.P10)       
+    }  
+  }
+    
+  
+   //% block="motorsInit"
+   //% block.loc.ru="Прогрев моторов"
+   //% weight=90
+  export function motorsInit( ) { 
+   pins.analogSetPeriod(AnalogPin.P9, 500)
+    pins.analogSetPeriod(AnalogPin.P16, 500)
+    pins.analogWritePin(AnalogPin.P9, 0)
+    pins.analogWritePin(AnalogPin.P16, 0)
+      
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)  
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)  
+  }
+    
+    //% block="Drive  left %left  right %right"
+    //% block.loc.ru="Движение  левый %left  правый %right"
+    //% left.min=-1023 left.max=1023
+    //% right.min=-1023 right.max=1023
+    //% weight=90
+  export function drive( left: number,  right: number): void {
+    let r = right
+    let l = left
+    l = Math.constrain(l,-1023,1023)
+    r = Math.constrain(r, -1023, 1023)
+   
+    if (l >= 0) {
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+    }
+    if (r >= 0) {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    }
+    pins.analogSetPeriod(AnalogPin.P9, 500)
+    pins.analogSetPeriod(AnalogPin.P16, 500)
+    pins.analogWritePin(AnalogPin.P9, Math.abs(l))
+    pins.analogWritePin(AnalogPin.P16, Math.abs(r))
+  }
+    
+  //% block="motorsStop "
+  //% block.loc.ru="Остановка моторов"
+  //% weight=90
+  export function motorsStop( ) { 
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)  
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)  
+    pins.analogSetPeriod(AnalogPin.P9, 500)
+    pins.analogSetPeriod(AnalogPin.P16, 500)
+    pins.analogWritePin(AnalogPin.P9, 0)
+    pins.analogWritePin(AnalogPin.P16, 0)
+  }
+    
+}
+
+    
+
+
+
